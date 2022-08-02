3c4dae1083004721d4677f6af56fc5c7bd5cab54
David Disu <daviddisu8@gmail.com>
2022-08-02 12:27:47 +0100

    View current working directory

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,19 @@
 import * as core from '@actions/core'
-import * as github from '@actions/exec'
-import { UnitTestRunner, assertLibCode } from '@remix-project/remix-tests'
+import * as exec from '@actions/exec'
 import * as fs from 'fs/promises'
 
 async function execute () {
-  const currentDirectory = process.cwd()
-  const sourceFolder = core.getInput('source-folder') || ''
-  const sourceFile = core.getInput('source-file') || ''
-  const testRunner = new UnitTestRunner()
+  const sourceFolder = core.getInput('source-folder')
+  const sourceFile = core.getInput('source-file')
+  const currentCompilerUrl = core.getInput('compiler-url')
 
+  exec.exec('ls')
   console.log('sourceFolder: ', sourceFolder)
   console.log('sourceFile: ', sourceFile)
-  console.log('currentDirectory: ', currentDirectory)
 
-  await core.group("Initialize Tests", async () => {
-    await testRunner.init()
-    const testFileContent = await fs.readFile(sourceFile, 'utf8')
-    const testDirContent = await fs.readdir(sourceFolder)
-
-    console.log('testFileContent: ', testFileContent)
-    console.log('testDirContent: ', testDirContent)
-  })
 
   await core.group("Run tests", async () => {
-    
+
   })
 }
 
