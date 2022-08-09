b3ac8b16ab35292360641784ed01f11f7259717f
David Disu <daviddisu8@gmail.com>
2022-08-09 10:08:18 +0100

    Use pre-patch build of libs

--- src/index.ts
+++ src/index.ts
@@ -18,12 +18,12 @@ async function execute () {
     const isNPMrepo = fs.existsSync(packageLock)
 
     if (isYarnRepo) {
-      await cli.exec('yarn', ['global', 'add', '@remix-project/remix-tests'])
+      await cli.exec('yarn', ['global', 'add', '@remix-project/remix-tests@0.2.16-alpha.0'])
     } else if (isNPMrepo) {
-      await cli.exec('npm', ['install', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', '@remix-project/remix-tests@0.2.16-alpha.0', '-g'])
     } else {
       await cli.exec('npm', ['init', '-y'])
-      await cli.exec('npm', ['install', '@remix-project/remix-tests', '-g'])
+      await cli.exec('npm', ['install', '@remix-project/remix-tests@0.2.16-alpha.0', '-g'])
     }
   })
 
